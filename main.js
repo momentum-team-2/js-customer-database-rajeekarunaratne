3e6bc0b76ccd710c46267d0c532f3e002303edbc
Rajee Karunaratne <rajee.karunaratne@gmail.com>
2020-06-03 08:56:27 -0400

    Tried to add momement

--- main.js
+++ main.js
@@ -3,22 +3,26 @@ for (let customer of customers){
     let customerName =  customer["name"]["first"] + ' ' + customer["name"]["last"]
     let customerAddress = customer["location"]["street"] + ' ' + customer["location"]["city"] + ' ' + customer["location"]["state"] + ' ' + customer["location"]["postcode"]
     let customerEmail = customer["email"]
-    let customerDOB = customer["DOB"]  
+    let customerDOB = customer["DOB: " + moment(customer.dob).format("MMM D, YYYY")] 
+    let customerSince = customer["Customer since: " + moment(customer.registered).format("MMM D, YYYY")] 
     let emailElem = document.createElement("p")
     let nameElem = document.createElement("h3")
     let cardElem = document.createElement("div")
     let addressElem = document.createElement("p")
     let dobElem = document.createElement("p")
+    let sinceElem = document.createElement("p")
     cardElem.innerHTML = cardElem.innerHTML + createImageElement(customer.picture.medium)
     nameElem.innerHTML = customerName;
     emailElem.innerHTML = customerEmail
     addressElem.innerHTML = customerAddress
     dobElem.innerHTML = customerDOB
+    sinceElem.innerHTML = customerSince
     cardElem.classList.add('card-bordered')
     cardElem.appendChild(nameElem)
     cardElem.appendChild(emailElem)
     cardElem.appendChild(addressElem)
     cardElem.appendChild(dobElem)
+    cardElem.appendChild(sinceElem)
     flexContainerElem.appendChild(cardElem);
 }
 
